8e87b5c557273e35727093804ee2318b556907c3
jrodriguezo <jrodriguezo.developer@gmail.com>
2024-11-09 18:15:39 +0100

    feat: remove fragment wrapper

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -20,45 +20,43 @@ function LoginForm({ onSubmit }: LoginFormProps) {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <CardContent>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                type="email"
-                id="email"
-                placeholder="Type your email"
-                {...register("email", { required: "Email is required" })}
-              />
-              {errors.email && (
-                <span className="text-red-500 text-sm">
-                  {errors.email.message?.toString()}
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="Type your password"
-                {...register("password", { required: "Password is required" })}
-              />
-              {errors.password && (
-                <span className="text-red-500 text-sm">
-                  {errors.password.message?.toString()}
-                </span>
-              )}
-            </div>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <CardContent>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Type your email"
+              {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message?.toString()}
+              </span>
+            )}
           </div>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button type="submit">Login</Button>
-        </CardFooter>
-      </form>
-    </>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Type your password"
+              {...register("password", { required: "Password is required" })}
+            />
+            {errors.password && (
+              <span className="text-red-500 text-sm">
+                {errors.password.message?.toString()}
+              </span>
+            )}
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button type="submit">Login</Button>
+      </CardFooter>
+    </form>
   );
 }
 
--- src/components/register-form/register-form.tsx
+++ src/components/register-form/register-form.tsx
@@ -20,64 +20,62 @@ function RegisterForm({ onSubmit }: RegisterFormProps) {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <CardContent>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input
-                id="name"
-                placeholder="Type your name"
-                {...register("name", { required: "Name is required" })}
-              />
-              {errors.name && (
-                <span className="text-red-500 text-sm">
-                  {errors.name.message?.toString()}
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                type="email"
-                id="email"
-                placeholder="Type your email"
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: {
-                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                    message: "Email is not valid",
-                  },
-                })}
-              />
-              {errors.email && (
-                <span className="text-red-500 text-sm">
-                  {errors.email.message?.toString()}
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="Type your password"
-                {...register("password", { required: "Password is required" })}
-              />
-              {errors.password && (
-                <span className="text-red-500 text-sm">
-                  {errors.password.message?.toString()}
-                </span>
-              )}
-            </div>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <CardContent>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="name">Name</Label>
+            <Input
+              id="name"
+              placeholder="Type your name"
+              {...register("name", { required: "Name is required" })}
+            />
+            {errors.name && (
+              <span className="text-red-500 text-sm">
+                {errors.name.message?.toString()}
+              </span>
+            )}
           </div>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button type="submit">Register</Button>
-        </CardFooter>
-      </form>
-    </>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Type your email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Email is not valid",
+                },
+              })}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message?.toString()}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Type your password"
+              {...register("password", { required: "Password is required" })}
+            />
+            {errors.password && (
+              <span className="text-red-500 text-sm">
+                {errors.password.message?.toString()}
+              </span>
+            )}
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button type="submit">Register</Button>
+      </CardFooter>
+    </form>
   );
 }
 
